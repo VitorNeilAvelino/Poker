b1e7100f2f4ccb555bf37c390bd9821b74a43444
Vítor Avelino <avelino.vitor@gmail.com>
2017-11-16 23:08:33 -0200

    Teste para Montar

---
+++ Poker.DominioTests/BaralhoTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Poker.Dominio.Tests
+{
+    [TestClass()]
+    public class BaralhoTests
+    {
+        [TestMethod()]
+        public void BaralhoTest()
+        {
+            var baralho = new Baralho();
+            var ouros = baralho.Cartas.Where(c => c.Naipe == Naipe.Ouros).ToList();
+            var naipes = baralho.Cartas.GroupBy(c => c.Naipe);
+
+            Assert.IsTrue(baralho.Cartas.Count == 52);
+            Assert.IsTrue(ouros.Count == 13);
+            Assert.IsTrue(naipes.Count() == 4);
+
+            var face = 1;
+
+            foreach (var ouro in ouros)
+            {
+                Assert.IsTrue((int)ouro.Face == face++);
+            }
+        }
+    }
+}
